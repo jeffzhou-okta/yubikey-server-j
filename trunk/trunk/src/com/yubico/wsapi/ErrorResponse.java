547df8674f7e52042c13f35d48c999c6605c1bfc
chenpaul <chenpaul@164f403b-c14f-0410-9192-65494835bddc>
2008-06-26 17:05:55 +0000

    1.0

---
+++ trunk/trunk/src/com/yubico/wsapi/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.yubico.wsapi;
+
+import java.util.Map;
+
+public class ErrorResponse extends Response {
+	ErrorResponse(Map map) {
+		super(map);
+	}
+
+	void putExtraInfo(String info) {
+		map.put(Constants.EXTRA_INFO, info);
+	}
+
+	public String toString() {
+		return "[ErrorResponse " + super.toString() + "]";
+	}
+}
